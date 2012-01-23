fb8e09cf0b2552786c2709351e46836ab6cfc26f
Luke Wilde <luke.wilde@clock.co.uk>
2012-01-23 23:59:18 +0000

    hello world.

--- app.js
+++ app.js
@@ -28,7 +28,9 @@ app.configure('production', function(){
 
 // Routes
 
-app.get('/', routes.index);
+app.get('/', function(req, res){
+	res.send('hello world');
+});
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
