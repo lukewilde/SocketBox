4d3fa1368c57cd63a64d527b59cb21ab5b059ec4
Luke Wilde <luke.wilde@clock.co.uk>
2012-01-24 00:12:06 +0000

    Customising output.

--- app.js
+++ app.js
@@ -28,9 +28,21 @@ app.configure('production', function(){
 
 // Routes
 
-app.get('/', function(req, res){
-	res.send('hello world');
-});
+app.get('/', routes.index);
+
+// var app = require('express').createServer(),
+// 	io = require('socket.io').listen(app);
+
+// // app.get('/', function (req, res) {
+// // 	res.sendfile(__dirname + '/index.html');
+// // });
+
+// io.sockets.on('connection', function (socket) {
+// 	socket.emit('news', { hello: 'world' });
+// 	socket.on('my other event', function (data) {
+// 		console.log(data);
+// 	});
+// });
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
