792b895280ac80d734bed56d6f35240b191c2635
Luke Wilde <luke.wilde@clock.co.uk>
2012-01-24 00:20:01 +0000

    Fixing socket io loading.

--- app.js
+++ app.js
@@ -27,22 +27,16 @@ app.configure('production', function(){
 });
 
 // Routes
+var io = require('socket.io').listen(app);
 
 app.get('/', routes.index);
 
-// var app = require('express').createServer(),
-// 	io = require('socket.io').listen(app);
-
-// // app.get('/', function (req, res) {
-// // 	res.sendfile(__dirname + '/index.html');
-// // });
-
-// io.sockets.on('connection', function (socket) {
-// 	socket.emit('news', { hello: 'world' });
-// 	socket.on('my other event', function (data) {
-// 		console.log(data);
-// 	});
-// });
+io.sockets.on('connection', function (socket) {
+	socket.emit('news', { hello: 'world' });
+	socket.on('my other event', function (data) {
+		console.log(data);
+	});
+});
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
